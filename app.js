fd1dfc0cad4cacfb02e9d3820b0804288a982e06
Kim Gorcyca <kgorcyca@gmail.com>
2019-04-17 18:02:30 -0700

    updated ajax

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Initial array of animals
-var animals = ["Pig", "Goat", "Dog"];
+var animals = [];
 
 
 // Function for displaying animal data
@@ -27,51 +27,52 @@ $("#add-animal").on("click", function(event) {
     animal = $("#animal-input").val().trim();
     animals.push(animal);
     animalButtons();
-    
-});
 
-// Calling the animalButtons function at least once to display the initial list of animals
-animalButtons();
+//This function adds on click event for the animal once they are chosen
+    $(".animal").on("click", function() {
+      animalGif = $(this).attr("data-name")
+      console.log("animal", animalGif);
 
-//onclick of the animal array items- will return one gif from giphy api
-$("button").on("click", function() {
-  var animalGif = $(this).attr("data-name")
-  console.log("animal", animalGif)
-
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    animalGif + "&api_key=dc6zaTOxFJmzC&limit=1";
-
-    $.ajax({
-       url: queryURL,
-       method: "GET"
-    })
+// calling the giphy api
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animalGif + "&api_key=dc6zaTOxFJmzC&limit=1";
+        
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
 
 // After the data comes back from the API
       .then(function(response) {
+        console.log("API", response)
         var results = response.data;
 
-        // Looping over every result item for rating
+// Looping over every result item for rating
         for (var i = 0; i < results.length; i++) {
           if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
             var gifDiv = $("<div>");
 
-            var rating = results[i].rating;
+            var rating = response.data[i].rating;
+            console.log("rating", rating)
 
             var p = $("<p>").text("Rating: " + rating);
 
-          // Creating an image tag
+// Creating an image tag
           var animalImage = $("<img>");
 
-          // Giving the image tag an src attribute of a proprty pulled off the
-          animalImage.attr("src", results[i].images.fixed_height.url);
+// Giving the image tag an src attribute of a proprty pulled off the
+            animalImage.attr("src", response.data[i].images.original.url);
 
-          // Appending the paragraph and personImage we created to the "gifDiv" div we created
-          gifDiv.append(p);
-          gifDiv.append(animalImage);
+// Appending the paragraph and personImage we created to the "gifDiv" div we created
+            gifDiv.append(p);
+            gifDiv.append(animalImage);
 
-          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-          $("#gifs-appear-here").prepend(gifDiv);
+// Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+            $("#gifs-appear-here").prepend(gifDiv);
         }
       }
     });
+      
+});
+
 });
